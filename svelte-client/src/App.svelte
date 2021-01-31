e33c924802e2724d804b6b7bbe844c0341b263e1
Stephen Chung <spchung95@gmail.com>
2021-01-31 15:08:54 -0800

    more stuff

--- svelte-client/src/App.svelte
+++ svelte-client/src/App.svelte
@@ -1,8 +1,10 @@
 <style>
+:global(:root) {
+  font-size: 16px;
+}
+
 main {
   text-align: center;
-  padding: 1em;
-  max-width: 240px;
   margin: 0 auto;
 }
 
@@ -12,60 +14,14 @@ h1 {
   font-size: 4em;
   font-weight: 100;
 }
-
-@media (min-width: 640px) {
-  main {
-    max-width: none;
-  }
-}
 </style>
 
 <script lang="ts">
-type Shareable = {
-  createdAt: Date;
-  content: string;
-  title?: string;
-};
-let inputContent: string = "";
-let shared: Shareable[] = [];
-
-const handleClick = () => {
-  // make sure shared content actually has stuff in it
-  if (inputContent.trim()) {
-    shared = [
-      ...shared,
-      {
-        createdAt: new Date(),
-        content: inputContent,
-      },
-    ];
-  }
-  inputContent = "";
-};
-
-const handleEnter = (e) => {
-  if (e.key === "Enter") {
-    handleClick();
-  }
-};
+import ShareContent from "./ShareContent.svelte";
 </script>
 
 <main>
   <h1>webclip</h1>
   <p>Share ephemeral information</p>
-  <div on:keydown="{handleEnter}">
-    <input type="text" bind:value="{inputContent}" />
-    <button on:click="{handleClick}">Share</button>
-  </div>
-  <div style="display: grid;
-					 grid-row-gap: 8px;">
-    {#each shared as { createdAt, content }}
-      <div>
-        {#if content.startsWith("http")}
-          <a href="{content}" target="_blank">{content}</a>
-        {:else}{content}{/if}
-        <i style="font-style: italic">({createdAt.toLocaleTimeString()})</i>
-      </div>
-    {/each}
-  </div>
+  <ShareContent />
 </main>
---
+++ svelte-client/src/ShareContent.svelte
@@ -0,0 +1,91 @@
+<style>
+h2 {
+  margin: 0;
+  padding: 0;
+}
+.humble-link {
+  color: blue;
+  cursor: pointer;
+}
+
+#clear-clipboard {
+  color: red;
+}
+
+section {
+  display: grid;
+  place-content: center;
+}
+
+.currently-shared-item {
+  text-align: left;
+  list-style-type: circle;
+  margin: 12px 0;
+}
+</style>
+
+<script lang="ts">
+type Shareable = {
+  updatedAt: Date;
+  content: string;
+  title?: string;
+};
+let inputContent: string = "";
+let shared: Shareable[] = [];
+
+const handleClick = () => {
+  // make sure shared content actually has stuff in it
+  if (inputContent.trim()) {
+    shared = [
+      ...shared,
+      {
+        updatedAt: new Date(),
+        content: inputContent,
+      },
+    ];
+  }
+  inputContent = "";
+};
+
+const handleClear = () => {
+  shared = [];
+};
+
+const handleEnter = (e) => {
+  if (e.key === "Enter") {
+    handleClick();
+  }
+};
+
+const copyToClipboard = (content: string): Promise<void> =>
+  navigator.clipboard.writeText(content);
+</script>
+
+<div class="controls" on:keydown="{handleEnter}">
+  <input type="text" bind:value="{inputContent}" />
+  <button on:click="{handleClick}">Share</button>
+  <button id="clear-clipboard" on:click="{handleClear}"
+    >Clear entire clipboard</button>
+</div>
+<section>
+  {#if shared.length}
+    <h2 style="font-weight: 500; margin: 3rem 0 0 0;">Currently sharing:</h2>
+  {/if}
+  <ul class="currently-shared">
+    {#each shared as { updatedAt, content }}
+      <li class="currently-shared-item">
+        <div>
+          {#if content.startsWith("http")}
+            <a href="{content}" target="_blank">{content}</a>
+          {:else}{content}{/if}
+          <div>
+            <span
+              class="humble-link"
+              on:click="{() => copyToClipboard(content)}">Copy</span>
+            <i>({updatedAt.toLocaleTimeString()})</i>
+          </div>
+        </div>
+      </li>
+    {/each}
+  </ul>
+</section>
